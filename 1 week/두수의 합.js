613952aadfeca372c5cd30b05535581ed8b825bd
Johnyun <108129068+yunyoungse2222@users.noreply.github.com>
2023-03-03 17:40:22 +0900

    두수의 합

---
+++ 1 week/두수의 합.js
@@ -0,0 +1,19 @@
+function solution(num1, num2) {
+  var answer = -1;
+  var answer = num1 + num2;
+  return answer;
+}
+
+// other
+function solution(num1, num2) {
+  if (-50000 <= num1 && num1 <= 50000) {
+    if (-50000 <= num2 && num2 <= 50000) {
+      const answer = num1 + num2;
+
+      return answer;
+    }
+  }
+}
+
+
+
