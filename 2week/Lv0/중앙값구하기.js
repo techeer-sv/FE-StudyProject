d4c8ea037bc7101be590ffc37774c3a5fb58d174
juwon5272 <jackson5272@naver.com>
2023-03-06 22:33:28 +0900

    중앙값 구하기

---
+++ 2week/Lv0/중앙값구하기.js
@@ -0,0 +1,13 @@
+// 중앙값 구하기
+// 문제 설명
+// 중앙값은 어떤 주어진 값들을 크기의 순서대로 정렬했을 때 가장 중앙에 위치하는 값을 의미합니다. 예를 들어 1, 2, 7, 10, 11의 중앙값은 7입니다.
+// 정수 배열 array가 매개변수로 주어질 때, 중앙값을 return 하도록 solution 함수를 완성해보세요.
+
+function solution(array) {
+  var answer = 0;
+  let arr = [];
+  arr = array.sort(function (a, b) {
+    return a - b;
+  });
+  return arr[Math.floor(arr.length / 2)];
+}
