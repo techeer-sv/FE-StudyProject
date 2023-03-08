02267d30c1ff037cf1d6b2f59e9804d9a383f313
juwon5272 <jackson5272@naver.com>
2023-03-08 22:28:56 +0900

    comment : 수박수박수박수박수박수?

---
+++ 김주원/2week/Lv1/수박수박수박수박수박수?.js
@@ -0,0 +1,15 @@
+// 수박수박수박수박수박수?
+// 문제 설명
+// 길이가 n이고, "수박수박수박수...."와 같은 패턴을 유지하는 문자열을 리턴하는 함수, solution을 완성하세요.
+// 예를들어 n이 4이면 "수박수박"을 리턴하고 3이라면 "수박수"를 리턴하면 됩니다.
+
+function solution(n) {
+  let watermelon = "수박";
+  // var answer = watermelon.repeat(Math.floor(n/2))
+  // var ans1 = n%2?answer+"수":answer;
+  // return ans1;
+
+  return n % 2
+    ? watermelon.repeat(Math.floor(n / 2)) + "수"
+    : watermelon.repeat(Math.floor(n / 2));
+}
