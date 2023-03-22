ab3d47d7d963cd9876b2e7f0da44476c3b82cca0
Johnyun <108129068+yunyoungse2222@users.noreply.github.com>
2023-03-22 12:22:10 +0900

    문자열 정수로 바꾸기

---
+++ 윤영세/4 week/문자열 정수로 바꾸기.js
@@ -0,0 +1,15 @@
+// mine
+function solution(s) {
+  var answer = parseInt(s);
+
+  return answer;
+}
+
+// other
+function strToInt(str) {
+  return str / 1;
+}
+
+function strToInt(str) {
+  return +str;
+}
