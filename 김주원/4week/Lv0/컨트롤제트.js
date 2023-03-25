dec499a452285c4cd9e02ea7ac8cb16d3e11822e
juwon5272 <jackson5272@naver.com>
2023-03-25 19:39:58 +0900

    컨트롤 제트

---
+++ 김주원/4week/Lv0/컨트롤제트.js
@@ -0,0 +1,18 @@
+// 컨트롤 제트
+// 문제 설명
+// 숫자와 "Z"가 공백으로 구분되어 담긴 문자열이 주어집니다. 문자열에 있는 숫자를 차례대로 더하려고 합니다.
+// 이 때 "Z"가 나오면 바로 전에 더했던 숫자를 뺀다는 뜻입니다.
+// 숫자와 "Z"로 이루어진 문자열 s가 주어질 때, 머쓱이가 구한 값을 return 하도록 solution 함수를 완성해보세요.
+
+function solution(s) {
+  var answer = 0;
+  let cut = s.split(" ");
+  for (let i = 0; i < cut.length; i++) {
+    if (cut[i] == "Z") {
+      answer -= parseInt(cut[i - 1]);
+    } else {
+      answer += parseInt(cut[i]);
+    }
+  }
+  return answer;
+}
