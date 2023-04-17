4f1de9b1d8e1515b16250a7ebc6d5c3d73b39f08
Johnyun <108129068+yunyoungse2222@users.noreply.github.com>
2023-04-17 16:49:50 +0900

    중복된 숫자 찾기

---
+++ 윤영세/7 week/중복된 숫자 찾기.js
@@ -0,0 +1,10 @@
+// mine
+function solution(s) {
+  let answer = "";
+  for (let i = 0; i < s.length; i++) {
+    if (s.indexOf(s[i]) === i) {
+      answer += s[i];
+    }
+  }
+  return answer;
+}
