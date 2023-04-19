2a627e087d9667f3da92865272cb4884a0ddc31a
Johnyun <108129068+yunyoungse2222@users.noreply.github.com>
2023-04-19 11:37:02 +0900

    팩토리얼

---
+++ 윤영세/8 week/펙토리얼.js
@@ -0,0 +1,15 @@
+function solution(n) {
+  let ans = 1;
+
+  for (let i = 1; i <= n; i++) {
+    ans *= i;
+
+    if (ans === n) {
+      return i;
+    }
+
+    if (ans > n) {
+      return i - 1;
+    }
+  }
+}
