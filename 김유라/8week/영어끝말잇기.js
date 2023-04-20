bc825ed4458d6f2f0e953a7f925bbdb74a918985
yura <rladbfk0000@naver.com>
2023-04-21 00:28:36 +0900

    comment : 영어 끝말잇기

---
+++ 김유라/8week/영어끝말잇기.js
@@ -0,0 +1,14 @@
+function solution(n, words) {
+  for (let i = 1; i < words.length; i++) {
+    if (words[i - 1].substr(-1) !== words[i][0]) {
+      return [(i % n) + 1, Math.ceil((i + 1) / n)];
+    } else {
+      for (let j = 0; j < i; j++) {
+        if (words[j] == words[i]) {
+          return [(i % n) + 1, Math.ceil((i + 1) / n)];
+        }
+      }
+    }
+  }
+  return [0, 0];
+}
