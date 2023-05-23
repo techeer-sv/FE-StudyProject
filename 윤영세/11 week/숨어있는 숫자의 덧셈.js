971f0c3a8dfdbeb1086f78b9d8cc9ba7a8597a1d
Johnyun <108129068+yunyoungse2222@users.noreply.github.com>
2023-05-23 19:09:47 +0900

    숨어있는 숫자의 덧셈

---
+++ 윤영세/11 week/숨어있는 숫자의 덧셈.js
@@ -0,0 +1,17 @@
+//other
+function solution(my_string) {
+  let result = 0;
+
+  for (let i = 0; i < my_string.length; i++) {
+    let tmp = 0;
+    while (!Number.isNaN(Number(my_string[i]))) {
+      tmp += my_string[i];
+      // console.log(tmp);
+      i++;
+    }
+    result += Number(tmp);
+    console.log(result);
+  }
+
+  return result;
+}
