8d949d668aac97bbecac263f6d96bbcda0dca111
Johnyun <108129068+yunyoungse2222@users.noreply.github.com>
2023-07-04 20:08:10 +0900

    삼총사

---
+++ 윤영세/14 week/삼총사.js
@@ -0,0 +1,29 @@
+function solution(number) {
+  let sum = 0;
+  for(let i = 0; i < number.length; i++){
+      for(let j = i+1; j < number.length; j++){
+          for(let k = j+1; k < number.length; k++)
+              if(number[i]+number[j]+number[k] === 0)
+                  sum += 1;
+      }
+  }
+  return sum;
+}
+
+//other
+function solution(number) {
+  let result = 0;
+
+  const combination = (current, start) => {
+      if (current.length === 3) {
+          result += current.reduce((acc, cur) => acc + cur, 0) === 0 ? 1 : 0;
+          return;
+      }
+
+      for (let i = start; i < number.length; i++) {
+          combination([...current, number[i]], i + 1);
+      }
+  }
+  combination([], 0);
+  return result;
+}
