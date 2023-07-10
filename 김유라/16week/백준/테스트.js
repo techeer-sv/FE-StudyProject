55857b790bd054525f8c98eb517971bdb735e860
yura <rladbfk0000@naver.com>
2023-07-11 01:43:39 +0900

    comment : 회문 문자열

---
+++ 김유라/16week/백준/테스트.js
@@ -0,0 +1,28 @@
+// let input = require("fs").readFileSync("예제.txt").toString().split("\n");
+// let fs = require("fs");
+
+// let count = Number(input[0]);
+// let numbers = [];
+
+// for (let i = 1; i < input.length; i++) {
+//   if (input[i] !== "") {
+//     numbers.push(input[i].split(" "));
+//   }
+// }
+
+// for (let i = 0; i < numbers.length; i++) {
+//   let num1 = Number(numbers[i][0]);
+//   let num2 = Number(numbers[i][1]);
+
+//   console.log(num1 + num2);
+// }
+let input = require("fs").readFileSync("/dev/stdin").toString().split("\n");
+let count = Number(input[0]);
+let answer = "";
+
+for (let i = 1; i <= count; i++) {
+  let num = input[i].split(" ");
+  answer += Number(num[0]) + Number(num[1]) + "\n";
+}
+
+console.log(answer);
