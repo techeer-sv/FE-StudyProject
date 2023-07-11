4ed90f703a3df0bf34554b24a02cecd58798c0c5
Johnyun <108129068+yunyoungse2222@users.noreply.github.com>
2023-07-11 15:12:20 +0900

    공배수

---
+++ 윤영세/16 week/공배수.js
@@ -0,0 +1,19 @@
+function solution(number, n, m) {
+  var answer = 0;
+  
+  if(number % n === 0){
+      if(number % m === 0){
+          answer = 1
+      }else{
+          answer = 0
+      }
+  }else{
+      answer = 0
+  }
+  return answer;
+}
+
+//other
+function solution(number, n, m) {
+  return (number%n ===0) ? (number%m===0) ? 1 : 0 : 0;
+}
