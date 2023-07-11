7ad232f15a523fd91abea545a271fad45145c25b
Johnyun <108129068+yunyoungse2222@users.noreply.github.com>
2023-07-11 15:12:14 +0900

    문자열로 변화

---
+++ 윤영세/16 week/문자열로 변환.js
@@ -0,0 +1,13 @@
+function solution(n) {
+  var answer = '';
+  answer = n + ''
+  return answer;
+}
+
+//other
+const solution = n => String(n)
+
+function solution(n) {
+  var answer = '';
+  return n.toString();
+}
